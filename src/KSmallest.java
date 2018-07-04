5145e15b3fa7324e498cce9efdc3b2bbe3651c9b
shuaizhang621 <sz1950@nyu.edu>
2018-07-03 17:38:41 -0700

    use quik select to solve k smallest problem

--- src/KSmallest.java
+++ src/KSmallest.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -32,5 +33,49 @@ public class KSmallest {
         }
         return result;
     }
+
+    public int[] kSmallestWithQuikSelect(int[] array, int k) {
+        if (array.length == 0 || k == 0) {
+            return new int[0];
+        }
+        quikSelect(array, 0, array.length - 1, k - 1);
+        int[] result = Arrays.copyOf(array, k);
+        Arrays.sort(result);
+        return result;
+    }
+
+    private void quikSelect(int[] array, int left, int right, int target) {
+        int mid = partition(array, left, right);
+        if (target == mid) {
+            return;
+        } else if (target < mid) {
+            quikSelect(array, left, mid - 1, target);
+        } else {
+            quikSelect(array, mid + 1, right, target);
+        }
+    }
+
+    private int partition(int[] array, int left, int right) {
+        int pivot = array[right];
+        int start = left;
+        int end = right - 1;
+        while (start <= end) {
+            if (array[start] < pivot) {
+                start++;
+            } else if (array[end] > pivot) {
+                end--;
+            } else {
+                swap(array, start++, end--);
+            }
+        }
+        swap(array, right, start);
+        return start;
+    }
+
+    private void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
 }
-}
+
