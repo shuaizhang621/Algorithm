210ff1f741dd44a560fd571d8c5721b989fdc4bc
ShuaiZhang <sz1950@nyu.edu>
2018-07-23 13:41:02 -0700

    Create GreyCode.java

---
+++ src/GreyCode.java
@@ -0,0 +1,16 @@
+class GreyCode {
+  // leetcode 89
+  // Time complexity O(2^n) n is the input.
+  // Space complexity O(1).
+  public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        result.add(0);
+        for (int i = 0; i < n; i++) {
+            int size = result.size();
+            for (int j = size - 1; j >= 0; j--) {
+                result.add(result.get(j) | 1 << i);
+            }
+        }
+        return result;
+    }
+}
