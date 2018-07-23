c9ec55d0cbea112fce47432b5e33d2a0c9546fe4
ShuaiZhang <sz1950@nyu.edu>
2018-07-23 13:38:23 -0700

    Create RightSideView.java
    2018-7-23 right side view of binary tree.

---
+++ src/RightSideView.java
@@ -0,0 +1,47 @@
+class RightSideView {
+  // BFS, when the tree is tall.
+  // Time complexity: O(n), Space complexity: O(n)
+  public List<Integer> rightSideView1(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+    Queue<TreeNode> q = new LinkedList<>();
+    q.offer(root);
+    while (!q.isEmpty()) {
+      int size = q.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode cur = q.poll();
+        if (cur.left != null) {
+          q.offer(cur.left);
+        }
+        if (cur.right != null) {
+          q.offer(cur.right);
+        }
+        if (i == size - 1) {
+          result.add(cur.val);
+        }
+      }
+    }
+    return result;
+  }
+  
+  // BFS, when the tree is wide. It can save space
+  // Time complexity: O(n), Space complexity: O(log(n))
+  public List<Integer> rightSideView2(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    rightSideView(root, 0, result);
+    return result;
+  }
+  
+  private void rightSideView(TreeNode root, int depth, List<Integer> result) {
+    if (root == null) {
+      return;
+    }
+    if (result.size() == depth) {
+      result.add(root.val);
+    }
+    rightSideView(root.right, depth + 1, result);
+    rightSideView(root.left, depth + 1, result);
+  }
+}
