3ac01659e11bb5c7f0d4087d44538143cd48728c
shuaizhang621 <sz1950@nyu.edu>
2018-07-25 01:23:35 -0700

    2018-7-25 product of array except self

---
+++ src/ProductOfArrayExceptSelf.java
@@ -0,0 +1,23 @@
+public class ProductOfArrayExceptSelf {
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null) {
+            return null;
+        }
+        int[] result = new int[nums.length];
+        if (nums.length == 0) {
+            return result;
+        }
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int temp = 1;
+        for (int k = nums.length - 2; k >= 0; k--) {
+            temp *= nums[k + 1];
+            result[k] *= temp;
+        }
+        return result;
+    }
+}
