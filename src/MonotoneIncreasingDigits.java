3161d3bd88a60fa5e25c571c2f21cdbbffc8fce8
shuaizhang621 <sz1950@nyu.edu>
2018-07-31 02:05:00 -0700

    2018-7-30 monotone increasing digits

---
+++ src/MonotoneIncreasingDigits.java
@@ -0,0 +1,14 @@
+public class MonotoneIncreasingDigits {
+    public int monotoneIncreasingDigits(int N) {
+        char[] array = Integer.toString(N).toCharArray();
+        for (int i = array.length - 1; i > 0; i--) {
+            if (array[i] < array[i - 1]) {
+                array[i - 1]--;
+                for (int j = i; j < array.length; j++) {
+                    array[j] = '9';
+                }
+            }
+        }
+        return Integer.valueOf(new String(array));
+    }
+}
